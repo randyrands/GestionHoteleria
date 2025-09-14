d1e97401015ed1d7b18825297c6ab1d3f2bae54d
Randy Salvador <randy.rand.inc@gmail,com>
2025-09-14 17:55:59 -0600

    Creacion del Mircroservicio Reservaciones

---
+++ ms-reservas/src/test/java/com/terry/reservas/MsReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terry.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
